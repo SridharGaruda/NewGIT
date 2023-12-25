6edce7631005e491c9e82df7f29012e90ab0d24b
SridharGaruda <sridhar.auminfo@gmail.com>
2023-12-25 18:05:54 +0530

    added log level niformation

--- src/main/java/testing/testlog.java
+++ src/main/java/testing/testlog.java
@@ -9,9 +9,10 @@ public class testlog {
 
 	public static void main(String[] args) {
 		
+		//Trace < debug < info < warn < error < fatal < off
 		
 		log = LogManager.getLogger(testlog.class);
-		log.fatal("This is a fatal log");
+		log.fatal("This is a fatal log get logged in app log");
 		log.error("this is error log");
 		log.warn("this is warn log");
 		log.info("this is info finally gulshana");
